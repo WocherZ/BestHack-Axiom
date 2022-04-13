525a7f52e03cfab61c115f69f348eca692a50a58
AlexEsn <saschaesn@gmail.com>
2022-04-13 19:23:02 +0300

    add_login

--- application/app/models.py
+++ application/app/models.py
@@ -1,19 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from django.core.validators import MinValueValidator, MaxValueValidator
-
-
-class User(models.Model):
-    email = models.EmailField()
-    password = models.CharField(max_length=64)
-    name = models.CharField(max_length=32)
-
-    balance = models.DecimalField(max_digits=19, decimal_places=2,  validators=[
-            MinValueValidator(0)
-        ],)
-
-    def __str__(self):
-        return str(self.name)
 
 
 class Stock(models.Model):
@@ -37,9 +25,8 @@ class Properties(models.Model):
                                  null=True)
     price = models.DecimalField(max_digits=19, decimal_places=2)
     number = models.IntegerField(validators=[
-            MinValueValidator(0)
-        ],)
+        MinValueValidator(0)
+    ], )
 
     def __str__(self):
         return str(str(self.user) + ": " + str(self.price))
-
--- application/app/views.py
+++ application/app/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import User, Stock, Properties
+from .models import *
 
-
-def home(request):
-    return render(request, 'home.html')
-
-
-def stocks(request):
-    return render(request, 'stocks.html')
+#
+# def home(request):
+#     return render(request, 'home.html')
+#
+#
+# def stocks(request):
+#     return render(request, 'stocks.html')
 
 
 def stock(request, stock_slug):
